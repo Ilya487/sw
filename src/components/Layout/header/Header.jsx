b13c55086381a0b3efc5874724af91827aaf915c
Ilya <ilaspica51@gmail.com>
2023-08-07 01:29:55 +0900

    add reset styles

--- src/components/Layout/header/Header.jsx
+++ src/components/Layout/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <NavLink to="people">People</NavLink>
       <NavLink to="search">Search</NavLink>
     </header>
