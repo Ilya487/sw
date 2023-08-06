a21813db2dd1a65dd42262ef8fdefe3771af5a8d
Ilya <ilaspica51@gmail.com>
2023-08-06 21:36:44 +0900

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import PeoplePage from "./pages/PeoplePage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import PersonPage from "./pages/PersonPage";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="people" element={<PeoplePage />} />
-        <Route path="people/:id" element={<PersonPage />} />
-        <Route path="/" element={<Navigate to="people" />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="people" element={<PeoplePage />} />
+          <Route path="people/:id" element={<PersonPage />} />
+          <Route path="/" element={<Navigate to="people" />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
