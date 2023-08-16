8ba68d342559909eca31eae546178675557cf442
Ilya <poctadladota@gmail.com>
2023-08-16 21:52:56 +0900

    add favorite btn on foundItem

--- src/components/Search/FoundElements/FoundItem/FoundItem.jsx
+++ src/components/Search/FoundElements/FoundItem/FoundItem.jsx
@@ -3,11 +3,16 @@ import { getEntityImg } from "../../../../utils/getEntityImg";
 import { Link } from "react-router-dom";
 import { handleImageError } from "../../../../utils/handleImageError";
 import styles from "./FoundItem.module.scss";
+import FavoriteBtn from "../../../UI/FavoriteBtn/FavoriteBtn";
+import { getEntityNumber } from "../../../../utils/getEntityNumber";
 
 const FoundItem = ({ item, entity }) => {
   return (
     <li>
-      <Link className={styles["search-item"]}>
+      <Link
+        className={styles["search-item"]}
+        to={`/${entity}/${getEntityNumber(item.url)}`}
+      >
         <img
           className={styles.img}
           src={getEntityImg(item.url, entity)}
@@ -17,6 +22,7 @@ const FoundItem = ({ item, entity }) => {
         <p className={styles.name}>
           {entity == "films" ? item.title : item.name}
         </p>
+        <FavoriteBtn item={item} />
       </Link>
     </li>
   );
