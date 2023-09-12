3a57e576950b8146a48e16a353c0662d29e26317
Ilya <poctadladota@gmail.com>
2023-09-13 00:28:20 +0300

    now favorite btn is svg

--- src/components/UI/FavoriteBtn/FavoriteBtn.jsx
+++ src/components/UI/FavoriteBtn/FavoriteBtn.jsx
@@ -2,27 +2,22 @@ import React from "react";
 import { useFavorite } from "../../../hooks/useFavorite";
 import styles from "./FavoriteBtn.module.scss";
 import clsx from "clsx";
+import { GrFavorite } from "react-icons/gr";
 
 const FavoriteBtn = ({ item, className, ...props }) => {
   const { toggleFavorite, checkFavorite } = useFavorite();
   const isChecked = checkFavorite(item.url);
 
-  return (
-    <button
-      {...props}
-      onClick={(e) => {
-        e.preventDefault();
-        toggleFavorite(item);
-      }}
-      className={clsx(
-        className,
-        styles.button,
+  const handleFavorite = (e) => {
+    e.preventDefault();
+    toggleFavorite(item);
+  };
 
-        isChecked ? styles["button--checked"] : styles["button--non-checked"]
-      )}
-    >
-      {isChecked ? "del" : "add"}
-    </button>
+  return (
+    <GrFavorite
+      onClick={handleFavorite}
+      className={clsx(styles.standart, className, isChecked && styles.active)}
+    />
   );
 };
 
