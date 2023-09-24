6643cab2ddcc16ecddac2e692fda8f79ca3a6d62
Ilya <poctadladota@gmail.com>
2023-09-24 16:01:38 +0300

    create cahce for getDetailedDescription

--- src/API/getDetailedDescription.js
+++ src/API/getDetailedDescription.js
@@ -1,4 +1,14 @@
+import { cache } from "../utils/cache";
+
 export async function getDetailedDescription(id, entity) {
-  const response = await fetch(`https://swapi.dev/api/${entity}/${id}`);
-  return await response.json();
+  const url = `https://swapi.dev/api/${entity}/${id}`;
+
+  if (url in cache) return cache[url];
+
+  const response = await fetch(url);
+  const json = response.json();
+
+  cache[url] = json;
+
+  return cache[url];
 }
