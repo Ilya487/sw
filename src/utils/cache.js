d2d2cad45611c82ac95e1e2a7e39fa3b4ca61e3a
Ilya <poctadladota@gmail.com>
2023-09-27 14:47:19 +0300

    now cache use cache API

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,3 +1,33 @@
+const CACHE_NAME = "queries cache";
+const STORAGE_NAME = "storage";
+
 export const cache = {
-  search: {},
+  _isInitialize: false,
+  _queryCache: undefined,
+
+  storage: {
+    search: {},
+  },
+
+  async initialize() {
+    if (this._isInitialize) return;
+
+    this._queryCache = await caches.open(CACHE_NAME);
+    const response = await this._queryCache.match(STORAGE_NAME);
+
+    if (response) {
+      const json = await response.json();
+
+      this.storage = json;
+    }
+
+    this._isInitialize = true;
+
+    window.addEventListener("beforeunload", async () => {
+      this._queryCache.put(
+        STORAGE_NAME,
+        new Response(JSON.stringify(this.storage))
+      );
+    });
+  },
 };
