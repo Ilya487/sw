199569cd6b2a7da813c0e84a4e02373472c38afa
Ilya <poctadladota@gmail.com>
2023-10-01 02:46:07 +0300

    now theme save after reload

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,10 +1,25 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { changeThemeCssVars } from "../utils/changeThemeCssVars";
 
 export const ThemeContext = createContext(null);
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "ligth");
+
+  useEffect(() => {
+    changeTheme(theme);
+  }, []);
+
+  useEffect(() => {
+    function saveTheme() {
+      localStorage.setItem("theme", theme);
+    }
+    window.addEventListener("beforeunload", saveTheme);
+
+    return () => {
+      window.removeEventListener("beforeunload", saveTheme);
+    };
+  }, [theme]);
 
   function changeTheme(theme) {
     setTheme(theme);
