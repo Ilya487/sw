752ae74e284861299bb3b7f654d5227c1836cc30
Ilya <poctadladota@gmail.com>
2023-10-01 03:57:01 +0300

    add styles for layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./header/Header";
+import styles from "./Layout.module.scss";
 
 const Layout = () => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
       <main>
         <Outlet />
       </main>
       <footer>footer</footer>
-    </>
+    </div>
   );
 };
 
