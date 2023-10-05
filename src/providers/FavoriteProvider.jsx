7bf64e9931d680559d54069a955e5632b0f01458
Ilya <poctadladota@gmail.com>
2023-10-05 15:25:37 +0300

    now favorites saves in locale storage

--- src/providers/FavoriteProvider.jsx
+++ src/providers/FavoriteProvider.jsx
@@ -1,9 +1,18 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const FavoriteContext = createContext(null);
 
 const FavoriteProvider = ({ children }) => {
-  const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState(getFromStorage() || []);
+
+  function getFromStorage() {
+    const str = localStorage.getItem("favorites");
+    return JSON.parse(str);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   const checkFavorite = (url) => {
     const index = favorites.findIndex((item) => item.url == url);
