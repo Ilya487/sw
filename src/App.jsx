f6b5d0064d29a13e330ed7fb2e32ca2c96ad03ae
Ilya <poctadladota@gmail.com>
2023-10-07 00:46:42 +0300

    created router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,16 @@
 import React from "react";
-import PeoplePage from "./pages/people/PeoplePage";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import PersonPage from "./pages/people/PersonPage/PersonPage";
-import Layout from "./components/Layout/Layout";
-import SearchPage from "./pages/search/SearchPage";
-import FilmsPage from "./pages/films/FilmsPage";
-import FilmPage from "./pages/films/FilmPage/FilmPage";
-import PlanetsPage from "./pages/PlanetsPage";
-import SpeciesPage from "./pages/SpeciesPage";
-import StarshipsPage from "./pages/StarshipsPage";
-import VehiclesPage from "./pages/VehiclesPage";
+import { RouterProvider } from "react-router-dom";
 import FavoriteProvider from "./providers/FavoriteProvider";
-import FavoritePage from "./pages/favorite/FavoritePage";
 import ThemeProvider from "./providers/ThemeProvider";
-import EntityDetailPage from "./components/EntityDetailePage/EntityDetailedPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import { router } from "./router/router";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <FavoriteProvider>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route path="people" element={<PeoplePage />} />
-              <Route path="people/:id" element={<PersonPage />} />
-
-              <Route path="search" element={<SearchPage />} />
-
-              <Route path="films" element={<FilmsPage />} />
-              <Route path="films/:id" element={<FilmPage />} />
-
-              <Route path="planets" element={<PlanetsPage />} />
-              <Route
-                path="planets/:id"
-                element={<EntityDetailPage entity="planets" />}
-              />
-
-              <Route path="species" element={<SpeciesPage />} />
-              <Route
-                path="species/:id"
-                element={<EntityDetailPage entity="species" />}
-              />
-
-              <Route path="starships" element={<StarshipsPage />} />
-              <Route
-                path="starships/:id"
-                element={<EntityDetailPage entity="starships" />}
-              />
-
-              <Route path="vehicles" element={<VehiclesPage />} />
-              <Route
-                path="vehicles/:id"
-                element={<EntityDetailPage entity="vehicles" />}
-              />
-
-              <Route path="favorite" element={<FavoritePage />} />
-
-              <Route path="/" element={<Navigate to="people?page=1" />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </FavoriteProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider>
+      <FavoriteProvider>
+        <RouterProvider router={router} />
+      </FavoriteProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/router/router.jsx
@@ -0,0 +1,87 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import PeoplePage from "../pages/people/PeoplePage";
+import PersonPage from "../pages/people/PersonPage/PersonPage";
+import Layout from "../components/Layout/Layout";
+import SearchPage from "../pages/search/SearchPage";
+import FilmsPage from "../pages/films/FilmsPage";
+import FilmPage from "../pages/films/FilmPage/FilmPage";
+import PlanetsPage from "../pages/PlanetsPage";
+import SpeciesPage from "../pages/SpeciesPage";
+import StarshipsPage from "../pages/StarshipsPage";
+import VehiclesPage from "../pages/VehiclesPage";
+import FavoritePage from "../pages/favorite/FavoritePage";
+import EntityDetailPage from "../components/EntityDetailePage/EntityDetailedPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+
+export const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    path: "/",
+    children: [
+      {
+        element: <PeoplePage />,
+        path: "people",
+      },
+      {
+        path: "people/:id",
+        element: <PersonPage />,
+      },
+      {
+        element: <SearchPage />,
+        path: "search",
+      },
+      {
+        element: <FilmsPage />,
+        path: "films",
+      },
+      {
+        element: <FilmPage />,
+        path: "films/:id",
+      },
+      {
+        element: <PlanetsPage />,
+        path: "planets",
+      },
+      {
+        path: "planets/:id",
+        element: <EntityDetailPage entity="planets" />,
+      },
+      {
+        element: <SpeciesPage />,
+        path: "species",
+      },
+      {
+        element: <EntityDetailPage entity="species" />,
+        path: "species/:id",
+      },
+      {
+        element: <StarshipsPage />,
+        path: "starships",
+      },
+      {
+        element: <EntityDetailPage entity="starships" />,
+        path: "starships/:id",
+      },
+      {
+        element: <VehiclesPage />,
+        path: "vehicles",
+      },
+      {
+        element: <EntityDetailPage entity="vehicles" />,
+        path: "vehicles/:id",
+      },
+      {
+        element: <FavoritePage />,
+        path: "favorite",
+      },
+      {
+        element: <Navigate to="people?page=1" />,
+        path: "/",
+      },
+      {
+        element: <NotFoundPage />,
+        path: "*",
+      },
+    ],
+  },
+]);
