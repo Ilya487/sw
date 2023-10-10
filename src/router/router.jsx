6b60b7cd42a3bb6b144f3d01c51d3d579fb48f9c
Ilya <poctadladota@gmail.com>
2023-10-10 19:02:51 +0300

    deploy app on gh-pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -13,75 +13,78 @@ import FavoritePage from "../pages/favorite/FavoritePage";
 import EntityDetailPage from "../components/EntityDetailePage/EntityDetailedPage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
-export const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    path: "/",
-    children: [
-      {
-        element: <PeoplePage />,
-        path: "people",
-      },
-      {
-        path: "people/:id",
-        element: <PersonPage />,
-      },
-      {
-        element: <SearchPage />,
-        path: "search",
-      },
-      {
-        element: <FilmsPage />,
-        path: "films",
-      },
-      {
-        element: <FilmPage />,
-        path: "films/:id",
-      },
-      {
-        element: <PlanetsPage />,
-        path: "planets",
-      },
-      {
-        path: "planets/:id",
-        element: <EntityDetailPage entity="planets" />,
-      },
-      {
-        element: <SpeciesPage />,
-        path: "species",
-      },
-      {
-        element: <EntityDetailPage entity="species" />,
-        path: "species/:id",
-      },
-      {
-        element: <StarshipsPage />,
-        path: "starships",
-      },
-      {
-        element: <EntityDetailPage entity="starships" />,
-        path: "starships/:id",
-      },
-      {
-        element: <VehiclesPage />,
-        path: "vehicles",
-      },
-      {
-        element: <EntityDetailPage entity="vehicles" />,
-        path: "vehicles/:id",
-      },
-      {
-        element: <FavoritePage />,
-        path: "favorite",
-      },
-      {
-        element: <Navigate to="people?page=1" />,
-        path: "/",
-      },
-      {
-        element: <NotFoundPage />,
-        path: "*",
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      path: "/",
+      children: [
+        {
+          element: <PeoplePage />,
+          path: "people",
+        },
+        {
+          path: "people/:id",
+          element: <PersonPage />,
+        },
+        {
+          element: <SearchPage />,
+          path: "search",
+        },
+        {
+          element: <FilmsPage />,
+          path: "films",
+        },
+        {
+          element: <FilmPage />,
+          path: "films/:id",
+        },
+        {
+          element: <PlanetsPage />,
+          path: "planets",
+        },
+        {
+          path: "planets/:id",
+          element: <EntityDetailPage entity="planets" />,
+        },
+        {
+          element: <SpeciesPage />,
+          path: "species",
+        },
+        {
+          element: <EntityDetailPage entity="species" />,
+          path: "species/:id",
+        },
+        {
+          element: <StarshipsPage />,
+          path: "starships",
+        },
+        {
+          element: <EntityDetailPage entity="starships" />,
+          path: "starships/:id",
+        },
+        {
+          element: <VehiclesPage />,
+          path: "vehicles",
+        },
+        {
+          element: <EntityDetailPage entity="vehicles" />,
+          path: "vehicles/:id",
+        },
+        {
+          element: <FavoritePage />,
+          path: "favorite",
+        },
+        {
+          element: <Navigate to="people?page=1" />,
+          path: "/",
+        },
+        {
+          element: <NotFoundPage />,
+          path: "*",
+        },
+      ],
+    },
+  ],
+  { basename: "/sw" }
+);
